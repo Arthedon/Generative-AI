dcea557c2be8358317eb12985d2abf8ba8e72a0e
Arthedon <deredel.arthur@gmail.com>
2024-05-21 21:56:13 +0200

    Add new network from Progressive GAN, fixing artefact issue on images (because of ConvTranspose2d) thanks to upsampling while improving stability

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-from network.WGAN import WGAN
+from network.ProGAN import ProGAN
 
 def main():
-    configuration = {"Discriminator": {"channels": [16, 64, 128, 256, 512, 2048]},
-                    "Generator": {"latentSpaceSize": 100, "channels": [4096, 1024, 512, 256, 128]},
-                    "Training": {"extraSteps": 5, "penaltyRatio": 10, "minLearningRate": 1e-7}}
-    network = WGAN(imageShape = (128, 128, 3), configuration = configuration)
-    network.train_gan(numEpochs = 35, learningRate = 1e-4, batchSize = 128, continueTraining = True)
+    configuration = {"Discriminator": {"channels": [128, 256, 256, 512, 512, 512]},
+                    "Generator": {"latentSpaceSize": 100, "channels": [512, 512, 512, 256, 256, 128]},
+                    "Training": {"extraSteps": 1, "penaltyRatio": 10, "epsilonDrift": 1e-3,
+                                 "epochUpscaling": [40, 80, 120, 160, 240], "epochAdaptation": 10,
+                                 "batchSizes": {"128": 8, "64": 32, "32": 128, "16": 256, "8": 1024, "4": 2048}}}
+    network = ProGAN(imageShape = (128, 128, 3), configuration = configuration)
+    network.train_gan(numEpochs = 161, learningRate = 1e-4, continueTraining = True, currentResolution = 64, currentEpoch = 170)
 
 if __name__ == "__main__":
     main()
